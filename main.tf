efefb3227b0ee9adba56afec1a4e660d804fd6b0
Ivan Junior <ivan_junior_we@hotmail.com>
2024-02-16 18:19:28 -0300

    Criando o RDS com engine MySQL.

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ terraform {
   }
 }
 
+resource "aws_db_instance" "rds_mysql" {
+  allocated_storage    = 10
+  db_name              = var.db_name
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = var.username
+  password             = var.password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
