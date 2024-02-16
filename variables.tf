ca4d3750d1826c0d6ca4163e4dfe8f99ebf2c060
Ivan Junior <ivan_junior_we@hotmail.com>
2024-02-16 18:19:51 -0300

    Criando variáveis.

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "key_name" {
+  default = "us-east-1_work"
+}
+
+variable "access_key" {
+}
+
+variable "tags_ManagedBy" {
+  default = "Terraform"
+}
+
+variable "username" {
+}
+
+variable "password" {
+}
+
+variable "db_name" {
+  default = "mydb"
+}
