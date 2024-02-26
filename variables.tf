531aae5fe03decae2d04e65a4e9634e47f3bb696
Ivan Junior <ivan_junior_we@hotmail.com>
2024-02-25 21:51:40 -0300

    Criando variáveis para configurações da VPC, subnet e SG.

--- variables.tf
+++ variables.tf
@@ -18,3 +18,19 @@ variable "password" {
 variable "db_name" {
   default = "mydb"
 }
+
+variable "vpc_cidr_block" {
+  default = "172.16.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  default = "172.16.10.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "sg_name" {
+  default = "sg_rds"
+}
