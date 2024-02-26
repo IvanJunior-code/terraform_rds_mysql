21b352e995ef6e219e4cb5a951270fbdec4d5964
Ivan Junior <ivan_junior_we@hotmail.com>
2024-02-25 21:50:49 -0300

    Criando Route Table e Route Table Association.

--- vpc.tf
+++ vpc.tf
@@ -63,3 +63,26 @@ resource "aws_internet_gateway" "gw_rds" {
 }
 ###################### ######## ####### ######################
 
+###################### Route Table ######################
+resource "aws_route_table" "route_table_rds" {
+  vpc_id = aws_vpc.vpc_rds.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_rds.id
+  }
+
+  tags = {
+    Name      = "Route Table"
+    ManagedBy = var.tags_ManagedBy
+  }
+}
+###################### ##### ##### ######################
+
+
+###################### Route Table Association ######################
+resource "aws_route_table_association" "association_subnet_route" {
+  subnet_id      = aws_subnet.subnet_rds.id
+  route_table_id = aws_route_table.route_table_rds.id
+}
+###################### ##### ##### ########### ######################
